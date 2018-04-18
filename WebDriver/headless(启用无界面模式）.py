622041dd897dd4fd502ef93b9ac9be70b6e5f12b
fabiokilling <36022541+fabiokilling@users.noreply.github.com>
2018-04-18 15:11:07 +0800

    Update headless(启用无界面模式）.py

--- WebDriver/headless(启用无界面模式）.py
+++ WebDriver/headless(启用无界面模式）.py
@@ -3,5 +3,5 @@ options = webdriver.ChromeOptions()
 options.set_headless()
 driver = webdriver.Chrome(options=options)
 #至此已完成
- driver.get('http://www.baidu.com')
- driver.page_source
+driver.get('http://www.baidu.com')
+driver.page_source
